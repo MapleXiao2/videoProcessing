898e26b87de2078c2ebd0760a0432105e406a98e
MapleX <2368479790@qq.com>
2022-03-01 20:26:56 +0800

    22.3.1

---
+++ src/main/java/com/maple/controller/UploadController.java
@@ -0,0 +1,30 @@
+package com.maple.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+public class UploadController {
+    @RequestMapping("/t1")
+    public void uploadTest(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws IOException {
+        System.out.println(file.getOriginalFilename());
+        //上传路径保存设置
+        String path = request.getServletContext().getRealPath("/upload");
+        File realPath = new File(path);
+        if (!realPath.exists()){
+            realPath.mkdir();
+        }
+        //上传文件地址
+        System.out.println("上传文件保存地址："+realPath);
+
+        //通过CommonsMultipartFile的方法直接写文件（注意这个时候）
+        file.transferTo(new File(realPath +"/"+ file.getOriginalFilename()));
+    }
+}
